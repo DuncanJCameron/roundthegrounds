7044ca02953dc29280cc5e2a824fce610828c3b0
DuncanJCameron <DuncanJCameron@outlook.com>
2020-02-26 16:31:59 +0000

    basic page

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+  echo "<!DOCTYPE html>";
+  echo "<html>";
+  echo "<head>";
+  echo "<title>Round the Grounds</title>";
+  echo "</head>";
+  echo "<body>";
+  echo "<div class='header'>";
+  echo "<img src='assets/banner.png'>";
+  echo "</div>";
+  echo "</body>";
+  echo "</html>";
+?>
